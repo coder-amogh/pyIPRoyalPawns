0c7d12fd022a7ffad5a13eccd187bf7d7be81bdb
coder-amogh <coderamogh@gmail.com>
2022-11-12 19:03:43 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = "0.2.2"
-DESCRIPTION = "UNOFFICIAL Python bindings for IPRoyal Pawns Dashboard API"
-LONG_DESCRIPTION = "A package that allows you to connect to IPRoyal Pawns API and interact with your data."
+VERSION = "1.0.0"
+DESCRIPTION = "UNOFFICIAL Python bindings for IPRoyal Pawns (Pawns.app) Dashboard API"
+LONG_DESCRIPTION = "A package that allows you to connect to IPRoyal Pawns (Pawns.app) API and interact with your data."
 
 # Setting up
 setup(
@@ -19,12 +19,12 @@ setup(
     long_description_content_type = "text/markdown",
     long_description = long_description,
     packages = find_packages(),
-    install_requires = ['requests', 'pySocks', 'beautifulsoup4', 'lxml'],
+    install_requires = ['requests', 'pySocks'],
     url = "https://github.com/coder-amogh/pyIPRoyalPawns",
     project_urls = {
         "Bug Tracker": "https://github.com/coder-amogh/pyIPRoyalPawns/issues",
     },
-    keywords = ['python', 'iproyal', 'ipr', 'passive income', 'iproyal pawns api', 'iproyal pawns dashboard', "python iproyal pawns"],
+    keywords = ['python', 'iproyal', 'ipr', 'passive income', 'iproyal pawns api', 'iproyal pawns dashboard', "python iproyal pawns", "pawns.app"],
     classifiers = [
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
